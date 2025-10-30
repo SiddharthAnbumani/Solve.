8f3d6d885eecd05a7593efd98d27660768e95a72
Siddharth Anbumani <161038518+SiddharthAnbumani@users.noreply.github.com>
2025-10-30 13:37:55 +0530

    Added Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar(){
+    return (
+        <div className="h-20 bg-gradient-to-r from-black via-zinc-900 to-zinc-950 border-b-2 border-black/10 flex items-center">
+            {/* <h1 className='text-3xl font-helvetica- text-zinc-300'>
+        Solve.
+        </h1> */}
+        <img src="public/solve.png" alt="" className="h-20" />
+        </div>
+    )
+}
